bd0bcc375b83352ec71e359ad34d2e5ef57ba7ec
jdosoudil <jiri.antonin.dosoudil@gmail.com>
2025-04-03 23:32:17 +0200

    Ještě znovu basePath

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: '/edupo-coolify',
-  // basePath: '/edupo-coolify'
+  basePath: '/edupo-coolify'
   // Your Next.js config here
 }
 
